dbeb1383294a6117b5a98e859455bf15b23a1498
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-10-04 20:36:05 +0200

    update

--- tools/update.sh
+++ tools/update.sh
@@ -5,5 +5,6 @@ repo=~/docs/
 cd "$repo"
 git pull origin main
 bundle exec jekyll build
+sudo rm -r /var/www/html/*
 sudo cp -R _site/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
