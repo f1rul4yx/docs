aecfbfdd31df95dbdfae0b7ccd52716865e038bf
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-10-04 23:29:05 +0200

    update

--- tools/update.sh
+++ tools/update.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
-repo=~/docs/
+
 export GEM_HOME="$HOME/gems"
 export PATH="$HOME/gems/bin:$PATH"
+repo=~/docs/
 
-{
-  echo "========== $(date) =========="
-  cd "$repo" || exit 1
-  git pull origin main
-
-  which bundle
-  bundle exec jekyll build 2>&1
-
-  ls -l _site | head -n 10
-
-  sudo rm -rf /var/www/html/*
-  sudo cp -R _site/* /var/www/html/
-  sudo chown -R www-data:www-data /var/www/html/
-} >> ~/update.log 2>&1
+cd "$repo"
+git pull origin main
+bundle exec jekyll build
+sudo rsync -av --delete _site/ /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/
